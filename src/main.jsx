86fe126155fa96c968037e8891a42dc325ddd2a7
sonalijoshi26 <sonalijoshi26012006@gmail.com>
2025-08-03 18:40:53 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+import Portfolio from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+createRoot(document.getElementById('root')).render(
+ 
+    <Portfolio />
+  
+)
