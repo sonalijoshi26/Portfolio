f191b3c2f58111bbde853c4f782ad9a3e2662015
sonalijoshi26 <sonalijoshi26012006@gmail.com>
2025-08-10 12:51:41 +0530

    new

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -2,9 +2,8 @@ export const Navbar=()=>{
     return <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
  <div className="max-w-5xl mx-auto px-4">
     <div className="flex justify-between items-center h-16">
-        <a href="">hi</a>
-        <a href="">hi</a>
-        <a href="">hi</a>
+        <a ></a>
+        hi 
     </div>
  </div>
     </nav>
