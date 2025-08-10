b5e19a9dcb8d7f49de409da3ccaeb1e4c97417e3
sonalijoshi26 <sonalijoshi26012006@gmail.com>
2025-08-10 12:38:34 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,16 @@
 import './index.css'
-
-function Portfolio() {
-
-  return ( <>    
-               <nav className="header">
-      <div className="logo">Sonal Joshi</div>
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#projects">Projects</a></li>
-        <li><a href="#skills">Skills</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  </>
+import { LoadingScreen } from './components/LoadingScreen';
+import { useState } from 'react';
+import { Navbar } from './components/sections/Navbar';
+function Portfolio(){
+  const [isLoaded,setIsLoaded]=useState(false)
+  return(
+    <>
+   {!isLoaded && <LoadingScreen onComplete={()=>setIsLoaded(true)}/>}{""}
+    <div className={`min-h-screen  transition-opacity duration-700 ${isLoaded ? "opacity-100" : "opacity-0"} bg-black text-gray-100`}>
+      <Navbar/>
+    </div>
+    </>
   )
 }
 export default Portfolio;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import Portfolio from './App.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
  
